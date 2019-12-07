b35f71ced17e7637a3c286ea200ada7d960e1e1f
Marochkin <vanek.marochkin@gmail.com>
2019-12-07 18:47:39 +0300

    laba10

---
+++ Laba10_error2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fib(int n){
+    
+    if (n<=2) return 1;
+    
+    return fib(n-1)+fib(n-2);
+}
+
+int main( {
+    
+    int n;
+    
+    while(scanf("%d", &n)!=EOF){
+        
+        printf("%d ", fib(n));
+    }
+    return 0;
+}
---
+++ Laba10_error4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fib(int n){
+    
+    
+    
+    return fib(n-1)+fib(n-2);
+}
+
+int main() {
+    
+    int n;
+    
+    while(scanf("%d", &n)!=EOF){
+        
+        printf("%d ", fib(n));
+    }
+    return 0;
+}
